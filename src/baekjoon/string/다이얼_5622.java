48207e24a851a9b7ee692a78587fdda2456f65bb
tsseo <tsseo.dev@gmail.com>
2021-03-27 13:09:29 +0900

    baekjoon 다이얼 5622번

---
+++ src/baekjoon/string/다이얼_5622.java
@@ -0,0 +1,11 @@
+package src.baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 다이얼_5622 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
