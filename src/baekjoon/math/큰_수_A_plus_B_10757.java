64bd923d5afdfb28bd62f11409a4629d1259a4f6
tsseo <tsseo.dev@gmail.com>
2021-04-17 23:53:26 +0900

    baekjoon 큰 수 A+B 2839번 해결

--- src/baekjoon/math/큰_수_A_plus_B_10757.java
+++ src/baekjoon/math/큰_수_A_plus_B_10757.java
@@ -3,20 +3,58 @@ package src.baekjoon.math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class 큰_수_A_plus_B_10757 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        char[][] rawData = new char[2][];
-        rawData[1] = bufferedReader.readLine().toCharArray();
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        int temp = 0;
+        int sum = 0;
 
-
-        while (stringTokenizer.hasMoreTokens()) {
-            System.out.println(stringTokenizer.nextToken());
-//            rawData[][] = stringTokenizer.nextToken().toCharArray();
+        int readCh = 0;
+        // 공백 나올때까지 stack1 push
+        while ((readCh = bufferedReader.read()) != 32) {
+            stack1.push(readCh - '0');
         }
-//        System.out.println(result);
+
+        // Enter 나올때까지 stack2 push
+        while ((readCh = bufferedReader.read()) != 10) {
+            stack2.push(readCh - '0');
+        }
+
+        // NAND -> 두 스택 모두 비었으면 멈춤
+        while (!(stack1.empty() && stack2.empty())) {
+            sum = 0;
+            if (!stack1.empty()) {
+                sum += stack1.peek();
+                stack1.pop();
+            }
+
+            if (!stack2.empty()) {
+                sum += stack2.peek();
+                stack2.pop();
+            }
+
+            sum += temp;
+
+            // 각 자릿수 합이 10 이상이면, 다음 합 계산에 +1 해주고 현재 자릿수 합에는 한자릿수만
+            if (sum >= 10) {
+                stringBuilder.insert(0, sum - 10);
+                temp = 1;
+            } else {
+                stringBuilder.insert(0, sum);
+                temp = 0;
+            }
+        }
+
+        if (temp == 1) {
+            stringBuilder.insert(0, 1);
+        }
+
+        System.out.println(stringBuilder.toString());
+
     }
 }
